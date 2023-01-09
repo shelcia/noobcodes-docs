c3d1561e1f8316b743ea6195e8fd82fb9343e2f6
shelcia <shelcia1999@gmail.com>
2023-01-09 12:50:32 +0530

    🎉 ✨ nextra init

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-const withNextra = require("nextra")({
-  theme: "nextra-theme-docs",
-  themeConfig: "./theme.config.jsx",
-  unstable_staticImage: true,
-});
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx',
+})
 
-module.exports = withNextra();
-
-// If you have other Next.js configurations, you can pass them as the parameter:
-// module.exports = withNextra({ /* other next.js config */ })
+module.exports = withNextra()
--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,17 @@
-export default {
-  logo: <span>NoobCode</span>,
+import React from "react";
+
+const config = {
+  logo: <span>Noobcodes</span>,
   project: {
-    link: "https://github.com/shuding/nextra",
+    link: "https://github.com/shelcia/noobcodes-docs",
   },
-  // ...
+  chat: {
+    link: "https://discord.com",
+  },
+  docsRepositoryBase: "https://github.com/shelcia/noobcodes-docs",
+  footer: {
+    text: "Noobcodes Docs",
+  },
 };
+
+export default config;
